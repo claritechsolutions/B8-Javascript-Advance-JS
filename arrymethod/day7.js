d9ca6e056c3343daa52c7f39d506f65e36aaf112
handepriyanka92@gmail.com <priyankahande92@gmail.com>
2022-04-26 11:03:17 +0530

    day7

---
+++ arrymethod/day7.js
@@ -0,0 +1,73 @@
+//const developerPosition = ["React Developer", "iOS Developer", "Java Developer", "Php Developer", "Backend Developer"];
+
+//convert array in to String
+//console.log(developerPosition.toString());
+
+//join array value and make array as string
+//console.log(developerPosition.join("~"));
+
+//access an array by its index
+//console.log("Third developer position -->",developerPosition[2]);
+
+//total length of arry
+//console.log("Total lenght-->",developerPosition.length);
+//console.log("Last element index-->", developerPosition.length-1);
+//console.log("Last element-->",developerPosition[99]);
+
+//find the index of an item in a array
+/*
+console.log("specific item index-->",developerPosition.indexOf("Php Developer"));
+
+if (developerPosition.indexOf("Php Developer")>=0){
+
+    console.log("yes php developer exist")
+}
+else{
+    console.log("no php developer not exist");
+
+
+}
+
+if (developerPosition.includes("iOS Developer"))
+{
+    console.log("yes exist");
+}
+else{
+    console.log("No exist");
+}*/
+
+//Appedn values in to array -Push
+//developerPosition.push("UI developer");
+//console.log(developerPosition.toString());
+//console.log(developerPosition);
+//console.log("Total length-->", developerPosition.length);
+
+
+//pop
+//developerPosition.pop();
+//console.log("deloperposition-->",developerPosition)
+
+//remove multiple items from the end of array-splice
+const developerposition = ["react developer","java developer", "ios developer","php developer","ui developer"];
+//console.log("Remain developer-->",developerposition.splice(3));
+//console .log("Remain developer-->",developerposition.splice(-2));
+
+//Remove first item from array
+//console.log("developerposition-->",developerposition.shift());
+
+//remove multiple item by start
+//const startElement = 0;
+//const deleteCount= 3;
+//console.log("developerposition-->",developerposition.splice(startElement,deleteCount));
+
+
+/*Add new first in to array*/
+//developerposition.unshift("Ajax developer");
+//console.log(developerposition);
+
+//developerposition.push("develops");
+//console.log(developerposition);
+
+
+
+
