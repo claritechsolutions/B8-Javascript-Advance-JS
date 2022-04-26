55ea0208dabd0c4ed7cf119c730673abce5b3db3
Siddharam-wadi <siddhuwadi@gmail.com>
2022-04-26 19:38:29 +0530

    file added

---
+++ array-method.js
@@ -0,0 +1,22 @@
+let subject=["mathamatics","physics","chemistry","english","hindi"]
+// console.log("convert to string-->",subject.toString());
+// console.log("make arrau as a string-->",subject.join(',  '));
+// console.log("find out type variable-->",typeof(subject.join(',  ')));
+// console.log("acess an index-->",subject[2]);
+// console.log("length of index-->",subject.length);
+// console.log("length of index-->",subject.length-1);
+// console.log("index of an item",subject.indexOf("hindi"));
+// console.log("check",subject.includes("hindi"));
+// console.log("append a value",subject.push("geography"));
+// console.log(subject);
+// subject.pop();
+// console.log(subject);
+// console.log("romove multiple-->",subject.splice(1));
+// console.log("remove frist items of array",subject.shift());
+// console.log("remove frist items of array",subject.unshift("biology"));
+
+//removing multiple item from begning
+// let start=1;
+// let end=2;
+// console.log("start to end",subject.splice(start,end));
+// console.log(subject);
