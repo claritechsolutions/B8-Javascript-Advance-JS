c1b60af2a99e7a2688dba827537c96402cbaa626
amruta deokate <amrutadeokate50@gmail.com>
2022-04-27 14:57:54 +0530

    loops

---
+++ loop.js
@@ -0,0 +1,30 @@
+for(i=0 ; i <=10 ; i++)
+{
+    console.log(i);
+    
+}
+let j=0;
+while(j<=10)
+{
+           j++
+           console.log(j)
+}
+let p = 0;
+do {
+  p += 1;
+  console.log(p);
+} while (p<=5)
+
+
+console.log(".....................")
+let a =['','','',''];
+console.log("Length of Array="+a.length);
+let b =new Array(5);
+console.log("Length of Array="+b.length);
+
+
+localStorage.setItem("name","amruta")
+localStorage.setItem("address","baramati")
+
+sessionStorage.setItem("name","amruta")
+sessionStorage.setItem("email","amruta@gmail.com")
