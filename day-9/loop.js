6e2d84b08465a596ddf6db7e9c4c21480de5b8f7
Akash-Edake <akashedake30@gmail.com>
2022-04-27 08:52:02 +0530

    day-9

---
+++ day-9/loop.js
@@ -0,0 +1,28 @@
+for (let i = 0; i<=10;i++){
+    console.log(i)
+}
+
+for(let j=0;j<+10;j++){
+    if(j==2){
+        break;
+    }
+    console.log(j)
+}
+
+for(let k=0;k<=10;k++){
+    if (k==2){
+        continue;
+    }
+    console.log(k)
+}
+let m=0;
+while(m<=10){
+console.log(m)
+m++
+}
+
+let n=1000;
+do {
+    console.log(n)
+    n++
+} while (n<=10);
---
+++ day-9/storeg.js
@@ -0,0 +1,9 @@
+localStorage.setItem("name","akash")
+localStorage.setItem("name",["akash","react_dev"])
+localStorage.setItem("mobile","866825XXXX")
+
+
+sessionStorage.setItem("name","akash")
+sessionStorage.setItem("name",["akash","react_dev"])
+sessionStorage.setItem("mobile","866825XXXX")
+
