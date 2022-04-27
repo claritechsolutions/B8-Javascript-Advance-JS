8ae726cec889b456edbba5177986520f9769b233
Shubham-7741 <shubhamsabale358@gmail.com>
2022-04-27 22:44:41 +0530

    loops

---
+++ loops/loops.js
@@ -0,0 +1,31 @@
+//For loop
+// for(let i=0; i<=10; i++){
+//  console.log(i)
+// }
+// for (let j = 1; ; j += 2) {
+//     console.log(j);
+//     if (j > 10) {
+//       break;
+//     }
+//   }
+  
+// const cars = ["BMW", "Volvo", "Saab", "Ford"];
+// let i, len, text;
+// for (i = 0, len = cars.length, text = "asdf"; i < len; i++) {
+//   text += cars[i] + "<br>";
+// }
+// console.log(cars);
+// console.log(i);
+
+// While Loop
+// let i = 0;
+// while (i < 10) {
+
+//     console.log(i);
+//     i++;
+// }
+let count = 1;
+while (count < 10) {
+    console.log(count);
+    count +=2;
+}
