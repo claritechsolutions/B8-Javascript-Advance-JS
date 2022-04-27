00c5fedb976fef692dfa28e4a334bd09477c2312
aaleshpatil22@gmail.com <aaleshpatil22@gmail.com>
2022-04-27 18:41:57 +0530

    newsplice

--- js.js
+++ js.js
@@ -1,1 +1,8 @@
-js.js
+let apple = ["A","B","C","D","E"];
+console.log(apple.splice(-2));
+console.log(apple);
+console.log(apple.concat(10,12));
+console.log(apple.shift());
+console.log(apple);
+// console.log(apple.splice(-2));
+// console.log(apple.splice(3,0,"kk","jj"));
