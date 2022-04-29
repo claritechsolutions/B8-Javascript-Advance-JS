e55f4a14361105e2d72520687902c51bdf1874cb
amruta deokate <amrutadeokate50@gmail.com>
2022-04-29 19:34:46 +0530

    function

---
+++ day-10/function.js
@@ -0,0 +1,14 @@
+function table(num){
+for( let i=0 ; i<=10 ;i++)
+{
+    console.log(`${num} X ${i} = ${num*i}`);
+
+}
+}
+console.log(".........table 12............")
+table(12);
+console.log(".........table 2............")
+table(2);
+console.log(".........table 9............")
+table(9);
+    
