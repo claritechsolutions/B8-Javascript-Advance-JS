3394f2ce4a2566d61e14e99ef29a4139fb478bf9
Akash-Edake <akashedake30@gmail.com>
2022-04-30 05:47:07 +0530

    day-10

---
+++ table.js
@@ -0,0 +1,31 @@
+// var people = [["John", "Smith"], ["Jane", "Doe"], ["Emily", "Jones"]]
+// console.table(people);
+
+function Person(firstName, lastName) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+  
+//   var john = new Person("John", "Smith");
+//   var jane = new Person("Jane", "Doe");
+//   var emily = new Person("Emily", "Jones");
+arraylist = [
+    {
+        name:"windows",
+        add:"microsoft"
+    },
+    {
+        name:"ubuntu",
+        add:"linux"
+    },
+    {
+        name:"mac os",
+        add:"apple"
+    },
+    {
+        name:"kali",
+        add:"linux"
+    }
+]
+  
+  console.table(arraylist);
