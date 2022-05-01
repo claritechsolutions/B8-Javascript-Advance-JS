4ec383d978234da0af00c823bb48c6e267fe8b18
amruta deokate <amrutadeokate50@gmail.com>
2022-05-01 18:25:39 +0530

    classes

---
+++ day-10/every.js
@@ -0,0 +1,8 @@
+let list =[1,2,3,4,5,6,7,8];
+let result = list.every(li);
+function li(value)
+{
+    
+   return value >5;
+}
+console.log(result);
---
+++ day-10/table.js
@@ -0,0 +1,23 @@
+const info =[
+    {
+         name: "Amruta",
+         Address: "Baramati",
+         Mark: 70
+    },
+    {
+         name: "Aarush",
+         Address: "Pune",
+         Mark: 50
+    },
+    {
+        name: "Aanandi",
+        Address: "Mumbai",
+        Mark: 76
+    },
+    {
+        name: "Aarya",
+        Address: "Satara",
+        Mark: 90
+    }
+];
+console.table(info);
