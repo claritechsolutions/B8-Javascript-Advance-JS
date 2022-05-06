aebeb9c7486e34b8002d8d31674626ea5b669574
amruta deokate <amrutadeokate50@gmail.com>
2022-05-06 14:12:09 +0530

    module

---
+++ module/library.js
@@ -0,0 +1,19 @@
+let Number=10;
+
+ function Addition(num1,num2){
+     let add=num1+num2;
+    return  add;
+    
+
+ }
+
+class information {
+            constructor(name,rollno,marks){
+                this.Sname=name;
+                this.Srollno=rollno;
+                this.Smarks=marks;
+             //   document.write(`Student Name is ${this.name}.<br> Student Roll number is ${this.rollno}.Student marks is ${this.marks}`);
+
+            }
+}
+export {Number,Addition,information};
---
+++ module/main.js
@@ -0,0 +1,7 @@
+import {Number,Addition,information} from './library.js';
+console.log(Number);
+
+console.log(Addition(20,50));
+
+let info =new information("amruta",10,50);
+console.log(info);
