382660913e1bde1c0d4b2321aa85157e266d52c1
pratiksha <pratikshanalawade2000@gmail.com>
2022-05-06 14:13:30 -0400

    Modules

---
+++ Day13/main.js
@@ -0,0 +1,6 @@
+import { name } from "./main2.js";
+console.log(`heloo I m ${name}`)
+import{add} from "./main2.js";
+console.log(add(19,2));
+import{word} from "./main2.js";
+let a=new word();
---
+++ Day13/main2.js
@@ -0,0 +1,10 @@
+export const name="pratiksha";
+
+export function add(x,y){
+    return x+y;
+}
+export class word{
+    constructor(){
+        console.log("I from satara")
+    }
+}
