43e805d4660ed4cb1ba3624fe555a97dbd9c2fdc
pratiksha <pratikshanalawade2000@gmail.com>
2022-05-09 11:01:31 -0400

    PromiseAll

---
+++ Day15/promiseall.js
@@ -0,0 +1,27 @@
+let cust = function (returnData,message){
+    return function(resolve,reject){
+        setTimeout(()=>{
+        console.log(` ${message}`)
+        resolve (returnData)
+    }, returnData*100)
+
+    }
+}
+let p1=new Promise(cust(10,"pratiksha"));
+let p2=new Promise(cust(20,"claritech"));
+// let p3 =new Promise((resolve,reject)=>{
+//     reject("The third promise is rejected")
+// })
+Promise.all([p1,p2])
+.then(
+    (result)=>{
+        console.log("Result-->",result)
+    }
+
+)
+.catch(
+    (error)=>{
+        console.log("Result-->",error)
+        
+    }
+)
