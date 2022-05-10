4217ed50f80e72e311359a736042bc5228570116
pratiksha <pratikshanalawade2000@gmail.com>
2022-05-10 11:12:15 -0400

    var_let_const

---
+++ Day16/var_let_const.js
@@ -0,0 +1,28 @@
+var name="electrical"
+var name="entc"
+name="mech"
+if(true){
+    var name="computer"
+    // console.log(name)
+}
+console.log("using var-->",name)
+/**Let */
+let firstName="Mayuri "
+let lastName="kadam"
+lastName="Palekar"
+if(true){
+    let firstName="Pratiksha"
+    let lastName="Nalawade"
+    console.log("Using let-->",firstName)
+    console.log("using let-->",lastName)
+}
+console.log("Using let-->",firstName)
+console.log("using let-->",lastName)
+
+/**constant */
+
+if(true){
+    const x="hello"
+    console.log("using const-->",x)
+}
+// console.log(x)
