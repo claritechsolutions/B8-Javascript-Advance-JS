5a57da57dc1ab7f14c2fff0ed59d70378e3f38ca
pratiksha <pratikshanalawade2000@gmail.com>
2022-05-12 04:09:01 -0400

    fetch operations

---
+++ Day18/fetchmethod.js
@@ -0,0 +1,52 @@
+
+/**POst Method-->Insert */
+// fetch('https://jsonplaceholder.typicode.com/posts',{
+//     method:'POST',
+//     body:JSON.stringify({
+//         title: 'foo',
+//     body: 'bar',
+//     userId: 1,
+//     }),
+//     headers: {
+//         'Content-type': 'application/json; ',
+//       },
+
+// })
+// .then((response)=>response.json())
+// .then((result)=>console.log(result))
+
+/**PUT Method-->Update*/
+// fetch('https://jsonplaceholder.typicode.com/posts/1',{
+//     method:'PUT',
+//     body:JSON.stringify({
+//         id: 1,
+//         title: 'foo',
+//         body: 'bar',
+//         userId: 1,
+
+//     }),
+//     headers:{
+//         'content-type' : 'application/json',
+//     },
+// })
+// .then((response)=>response.json())
+// .then((result)=>document.write(result))
+
+/**PATCH Method-->Update partial Value */
+// fetch('https://jsonplaceholder.typicode.com/posts/2',{
+//     method:'PATCH',
+//     body:JSON.stringify({
+//         title: 'foo'
+//     }),
+//     headers:{
+//         'content-type':'application/json'
+//     },
+
+// })
+// .then((response)=>response.json())
+// .then((result)=>console.log(result))
+
+/**Delete Method */
+// fetch('https://jsonplaceholder.typicode.com/posts/1', {
+//   method: 'DELETE',
+// });
