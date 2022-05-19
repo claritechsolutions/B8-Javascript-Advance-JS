eb9d2eddb9a8d3af15891bb6419309bcc2633137
Akshada08 <akshadadherange@gmail.com>
2022-05-19 20:29:33 +0530

    inheritance

---
+++ Javascript/inheritance.js
@@ -0,0 +1,26 @@
+
+// car is parent class
+// properties and methods in constructor
+class Car {
+  constructor(brand) {
+    this.carname = brand;
+  }
+  present() {
+    return 'I have a ' + this.carname;
+  }
+}
+
+class Model extends Car {
+  constructor(brand, mod) {
+    //   super refers to parent class
+    super(brand);
+    this.model = mod;
+  }
+  show() {
+    return this.present() + ', it is a ' + this.model;
+  }
+}
+
+let myCar = new Model("Ford", "Mustang");
+console.log('myCar.show()');
+
