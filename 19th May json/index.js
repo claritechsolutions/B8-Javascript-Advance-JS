e090528008243424dab31b321063a6bec764ece0
AratiChitte <aratithakare@gmail.com>
2022-05-20 20:09:08 +0530

    json- crud operations

---
+++ 19th May json/index.js
@@ -0,0 +1,76 @@
+function getinfo() {
+    fetch('http://localhost:3000/employees')
+        .then(response => response.json())
+        .then(json => {
+            // console.log(json);
+            for (i in json) {
+                document.getElementById("details").innerHTML += (`<tr><td>${json[i].id}</td>
+            <td>${json[i].name}</td>
+        <td>${json[i].email}</td>
+        <td>${json[i].address}</td>
+        </tr>`);
+            }
+        })
+}
+
+function postinfo() {
+    let options = {
+        method: 'POST',
+        body: JSON.stringify({
+            id: 6,
+            name: "Swapnali",
+            email: "swapnaliP@gmail.com",
+            designation: "Mumbai"
+        }),
+        headers: {
+            'Content-type': 'application/json',
+        },
+    }
+    fetch('http://localhost:3000/employees', options)
+        .then((response) => response.json())
+        .then((json) => console.log(json));
+}
+
+function putinfo() {
+    let options = {
+        method: 'PUT',
+        body: JSON.stringify({
+            id: 4,
+            name: "Tejas",
+            email: "Tejas19@gmail.com",
+            address: "Kolkata"
+        }),
+        headers: {
+            'Content-type': 'application/json',
+        },
+    }
+    fetch('http://localhost:3000/employees/4', options)
+        .then((response) => response.json())
+        .then((json) => console.log(json));
+}
+
+function patchinfo() {
+    let options = {
+        method: 'PATCH',
+        body: JSON.stringify({
+            name: "Ketan",
+        }),
+        headers: {
+            'Content-type': 'application/json',
+        },
+    }
+    fetch('http://localhost:3000/employees/4', options)
+        .then((response) => response.json())
+        .then((json) => console.log(json));
+}
+
+
+function deleteinfo() {
+    let option = {
+        method: 'DELETE',
+    }
+
+    fetch('http://localhost:3000/employees/6', option)
+        .then((response) => response.json())
+        .then((json) => console.log(json));
+}
--- 6th May/prom.js
+++ 6th May/prom.js
@@ -12,20 +12,24 @@
 // let onfail=(error)=>{console.log(error)};
 // prom(true).then(onfulfil).catch(onfail);
 
-function prom(a,b){return new Promise(function(resolve,reject){
-    console.log("Fetching data, please wait");
-    let c=a%b;
-    setTimeout(()=>{if(c==0){
-        resolve(`${a} is divisible by ${b} `);
-    }
-    else{
-        reject(`${a} is not divisible by ${b} `);
-    }},2000)
-});
+function prom(a, b) {
+    return new Promise(function (resolve, reject) {
+        console.log("Fetching data, please wait");
+        let c = a % b;
+        setTimeout(() => {
+            if (c == 0) {
+                resolve(`${a} is divisible by ${b} `);
+            }
+            else {
+                reject(`${a} is not divisible by ${b} `);
+            }
+        }, 2000)
+    });
 }
+
 
 // let onfulfil=(result)=>{console.log(result)};
 // let onfail=(error)=>{console.log(error)};
 // prom(true).then(onfulfil).catch(onfail);
 
-prom(8,2).then((result)=>{console.log(result)}).catch((error)=>{console.log(error)});
+prom(8, 2).then((result) => { console.log(result) }).catch((error) => { console.log(error) });
