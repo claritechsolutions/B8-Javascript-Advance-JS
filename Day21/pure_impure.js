d95e8f1a7291a76205c9e7f2b42adaf1c5f06b73
pratiksha <pratikshanalawade2000@gmail.com>
2022-05-20 13:17:41 -0400

    pure_impure

---
+++ Day21/pure_impure.js
@@ -0,0 +1,38 @@
+/**
+ * Impure Function
+ */
+
+// var x = 10;
+
+// function add(x) {
+//   return x++;
+// }
+
+// console.log(add());
+// console.log(add());
+// console.log(add());
+// console.log(add());
+// const myNames = ["Oluwatobi", "Sofela"];
+
+// function updateMyName(newName) {
+//   myNames.push(newName);
+//   return myNames;
+// }
+//updateMyName()
+//console.log(updateMyName)
+/**
+ * Pure function
+ * 
+ * Explaination:
+ * Pure function are functions that accept an input and return a value without modifying any data 
+ * outside its scopes.
+ */
+
+ function add(x) {
+    return x + 1;
+  }
+  
+  add(10);
+  add(11);
+  add(12); 
+  console.log(add)
