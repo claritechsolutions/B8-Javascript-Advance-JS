539b528f950f04bba718c2ceca8cfbf989c071e1
pratiksha <pratikshanalawade2000@gmail.com>
2022-05-29 21:00:41 -0400

    jsx

---
+++ React1/my-app/src/Componants/Jsxex.js
@@ -0,0 +1,10 @@
+import React from 'react';
+const Artist=()=>{
+    return(
+        <div>
+            <h3 style={{color: "blue"}}>This is using JSX</h3>
+            <h2 > We are Devlopers</h2>
+        </div>
+    )
+}
+export default Artist
