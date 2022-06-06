b6893480548345dd20b814c21e27790ea779bb6d
darshangaikwad123 <darshangaikwad2499@gmail.com>
2022-06-06 08:35:43 +0530

    all file

---
+++ DOM manipulation/getmethods.js
@@ -0,0 +1,39 @@
+// how doument get ==>1.HTML
+//                    2.Text
+//                    3.Attributes
+
+// DOM GETS METHODS=======>
+
+// 1.innerText
+// 2.innnerHTML
+// 3.getAttribute
+// 4.getAttributeNode
+// 5.Attributes
+
+// 1.innerText
+
+// var element;
+// element=document.getElementById("header").innerText
+// console.log(element)
+
+// 2.innerHTML
+// var element;
+// element=document.getElementById("header").innerHTML
+// console.log(element)
+
+//3.get Attribute==>it is give the id,class name
+
+// var element;
+// element=document.getElementById("header").getAttribute("class")
+// console.log(element)
+
+// 4.getAttributeNode()
+
+// var element;
+// element=document.getElementById("header").getAttributeNode("id").value
+// console.log(element)
+
+// 5.Attributes
+// var element;
+// element=document.getElementById("header").attributes[2].value
+// console.log(element)
---
+++ DOM manipulation/mainsetmethod.js
@@ -0,0 +1,47 @@
+// // ========>1.document
+// var element;
+// element=document;
+// console.log(element);
+// // ========>2.document.all[]:- [] array used target perticular Element
+// var element;
+// element=document.all;
+// console.log(element);
+// var element;
+// element=document.all[9];
+// console.log(element);
+// // ========>3.document.tagName
+// element=document.body;
+// console.log(element)
+// element=document.title;
+// console.log(element)
+// element=document.body;
+// console.log(element)
+//// ========>4.document.links:- [] array used target perticular Element
+// var element;
+// element=document.links[1]
+// console.log(element)
+//// ========>4.document.images:- [] array used target perticular Element
+// var element;
+// element=document.images[0]
+// console.log(element)
+//// ========>5.document.doctype
+// var element;
+// element=document.doctype
+// console.log(element)
+//// ========>6.document.URL
+// var element;
+// element=document.URL
+// console.log(element)
+// //// ========>6.document.domain
+// var element;
+// element=document.domain
+// console.log(element)
+// //// ========>7.document.baseURI
+// var element;
+// element=document.baseURI
+// console.log(element)
+
+
+
+
+
