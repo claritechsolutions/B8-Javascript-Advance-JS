9a6b8517b6812dd91773b3d0e9d0674e5f698029
pratiksha <pratikshanalawade2000@gmail.com>
2022-08-10 11:43:15 -0400

    js practice

---
+++ Practice/practice.js
@@ -0,0 +1,54 @@
+// let myArry=['eat ','sleep']
+// myArry.push('exercise')
+// console.log(myArry)
+// let myArry=['eat ','sleep','exercise']
+// myArry.pop('eat')
+// console.log(myArry)
+
+ /*let myArry=[11,12,100,21,13,41,56,30]
+ myArry.sort((a,b)=>{
+        return a-b
+ })
+ console.log(myArry.reverse())
+
+let array=[1,2,4,5]
+var a = array.concat(myArry)
+console.log(a)
+console.log(myArry)
+
+let b=[1,2,3,4]
+
+console.log(b.join('*'))
+
+let c=[6,7,8,9]
+console.log("last element-->",c[c.length-1])*/
+ 
+// let d=[4,8,9,10,56]
+// console.log("find-->",d.every((a)=>
+//         a>2
+
+// ))      
+
+// let a=(num1)=> (num2)=>(num3)=>{
+//     return num1+num2+num3
+// }
+// var b=a(10)(20)(30)
+
+// console.log(b) //curry function
+
+// function add(num1){
+//     return function (num2){
+//         return function(num3){
+//             return num1+num2+num3
+            
+//         }
+//     }
+// }
+// var b=add(10)(20)(30)
+// console.log(b)
+// let a=[1,3,5,8]
+// console.log("toString-->",a.isArray(a))
+
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+let result =  Array.isArray(fruits);
+console.log(result)
