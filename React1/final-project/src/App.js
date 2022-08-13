a5abec3e3ae1dbf17a9d2b3de6ff19c20cc544ad
pratiksha <pratikshanalawade2000@gmail.com>
2022-08-13 04:33:21 -0400

    e-commerece design part

---
+++ React1/final-project/src/App.js
@@ -0,0 +1,24 @@
+//import logo from './logo.svg';
+//import './App.css';
+
+ import Home from "./Pages/Home";
+// import Product from "./Pages/Product";
+//import Cart from "./Pages/Cart";
+//import Login from "./Pages/Login";
+//import Register from "./Pages/Register";
+// import ProductList from "./Pages/ProductList";
+
+function App() {
+  return (
+    <div className="App">
+    <Home/> 
+    {/* <ProductList/> */}
+    {/* <Product/> */}
+    {/* <Register/> */}
+    {/* <Login/> */}
+    {/* <Cart/> */}
+    </div>
+  );
+}
+
+export default App;
---
+++ React1/final-project/src/Pages/Home.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import Announcement from '../Components/Announcement'
+import Categories from '../Components/Categories'
+import Navbar from '../Components/Navbar'
+import Slider from '../Components/Slider'
+import Products from '../Components/Products'
+import Newsletter from '../Components/Newsletter'
+import Footer from '../Components/Footer'
+const Home = () => {
+    return (
+        <div>
+            <Announcement></Announcement>
+            <Navbar></Navbar>
+            <Slider/>
+            <Categories/>
+            <Products></Products>
+            <Newsletter/>
+            <Footer/>
+        </div>
+    )
+}
+
+export default Home
